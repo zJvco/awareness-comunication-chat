18564b066a8527a0a0a1ad0db138c12571ca7d1a
AlexdeJesusFS <65736748+AlexdeJesusFS@users.noreply.github.com>
2022-04-23 09:55:31 -0300

    Add files via upload

---
+++ Main/Client.py
@@ -0,0 +1,42 @@
+import threading
+import socket
+
+
+class Client:
+    def __init__(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            client.connect(('localhost', 7777))
+        except:
+            print('\nNão foi possívvel se conectar ao servidor!\n')
+
+        username = input('Usuário> ')
+        print('\nConectado')
+
+        thread1 = threading.Thread(target=self.receiveMessages, args=[client])
+        thread2 = threading.Thread(target=self.sendMessages, args=[client, username])
+
+        thread1.start()
+        thread2.start()
+
+    def receiveMessages(client):
+        while True:
+            try:
+                msg = client.recv(2048).decode('utf-8')
+                print(msg + '\n')
+            except:
+                print('\nNão foi possível permanecer conectado no servidor!\n')
+                print('Pressione <Enter> Para continuar...')
+                client.close()
+                break
+
+    def sendMessages(client, username):
+        while True:
+            try:
+                msg = input('\n')
+                client.send(f'<{username}> {msg}'.encode('utf-8'))
+            except:
+                return
+
+client = Client()
